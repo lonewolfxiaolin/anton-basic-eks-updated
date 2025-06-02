c8c4941f79abfffee6ab654d06fa5bd867053c14
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-06-01 17:43:35 -0700

    auto set context

--- 11-runtime.tf
+++ 11-runtime.tf
@@ -4,7 +4,7 @@ resource "null_resource" "update_kubeconfig" {
   count = var.enable_kubeconfig ? 1 : 0
 
   provisioner "local-exec" {
-    #interpreter = ["/bin/bash", "-c"]
+    #interpreter = ["/usr/bin/env", "bash"]
     command     = "aws eks update-kubeconfig --region ${var.region} --name ${aws_eks_cluster.demo.name}"
   }
 
