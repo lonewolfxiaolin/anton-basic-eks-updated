d0e4f3fad9c1c391f0e0d20be569325cbcb4110f
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-06-30 15:23:53 -0700

    minor code refactor

--- 1-auth.tf
+++ 1-auth.tf
@@ -19,9 +19,17 @@ terraform {
 
   }
 
+  backend "s3" {
+    bucket  = "terraform-state-aaronmcd" # Name of the S3 bucket
+    key     = "052525.tfstate"           # The name of the state file in the bucket
+    region  = "us-east-2"                # Use a variable for the region
+    encrypt = true                       # Enable server-side encryption (optional but recommended)
+  }
+}
+
 
 provider "aws" {
-  region  = "us-east-1"
+  region  = var.region
   profile = "default"
 }
 
--- 1b-auth-backend.tf
+++
@@ -1,7 +0,0 @@
-  backend "s3" {
-    bucket  = "terraform-state-aaronmcd" # Name of the S3 bucket
-    key     = "052525.tfstate"           # The name of the state file in the bucket
-    region  = "us-east-2"                # Use a variable for the region
-    encrypt = true                       # Enable server-side encryption (optional but recommended)
-  }
-}
